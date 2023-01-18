5e7f26ac98268af3878a83ccc8c1fd1875e36df4
Rober261 <robertodifu@gmail.com>
2023-01-18 17:17:08 +0000

    Enrutamiento

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Este programa no usa framework pero usa MVC con las siguientes librerias:
 
 - philo/laravel-blade para la gestion de plantillas
 - iluminate/database para el ORM
+- autoload para el enrutamiento
---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php 
+require_once '../vendor/autoload.php';
+
+
+//Router System
+$router = new AltoRouter();
+
+
+//List of routes
+$router->map('GET','/', function(){
+  echo "hola enrutamiento";
+});
+
+//End of List
+
+$match = $router->match();
+if($match) {
+ $target = $match["target"];
+ if(is_string($target) && strpos($target, "#") !== false) {
+     list($controller, $action) = explode("#", $target);
+     $controller = "Controller\\" . $controller;
+     $controller = new $controller;
+     $controller->$action($match["params"]);
+ } else {
+     if(is_callable($match["target"])) call_user_func_array($match["target"], $match["params"]);
+     else require $match["target"];
+ }
+} else {
+ echo "Ruta no válida";
+ die();
+}
+
+?>
